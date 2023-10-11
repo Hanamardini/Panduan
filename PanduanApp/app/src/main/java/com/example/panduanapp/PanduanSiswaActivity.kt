9c8973a1cc1d50143d60c9fd738c30ea7b3d12a8
Hanamardini <hanamp90@gmail.com>
2023-10-11 15:07:23 +0700

    panduan guru dan siswa

---
+++ PanduanApp/app/src/main/java/com/example/panduanapp/PanduanSiswaActivity.kt
@@ -0,0 +1,11 @@
+package com.example.panduanapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PanduanSiswaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_panduan_siswa)
+    }
+}
